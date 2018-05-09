4d2c8e554598dc5c4b1c445ad1bb8991ec7c4fdf
kluge121 <kluge0221@gmail.com>
2018-05-09 19:16:22 +0900

    basic express

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+var hostname = 'ec2-52-78-135-92.ap-northeast-2.compute.amazonaws.com';
+var port = 3000;
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(port, hostname, function () {
     console.log('Example app listening on port 3000!');
 });
 
