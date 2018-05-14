0d2e49c65bf98b226efb8d6112ada39d3cc0f33d
kluge121 <kluge0221@gmail.com>
2018-05-14 16:23:15 +0900

    mysql connect

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
-
 const app = express();
 const host = 'ubuntu@ec2-52-78-135-92.ap-northeast-2.compute.amazonaws.com';
+const config = require('db_info').local;
+const testQuery = 'Select * From User';
+
 
 app.get('/', (req, res) => {
     res.send("dd");
 });
 
+
 app.listen(3000, host, () => {
     console.log("dd2");
+    console.log(config);
 });
+
+
+function callback(err, result) {
+    if (err) {
+        throw err
+    }
+    console.log("Selsect Complete");
+    console.log(query.sql);
+
+}
+
 
