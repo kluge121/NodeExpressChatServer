e77ad1a43b5e5e4ccd0a269e0e89dfc39647904d
kluge121 <kluge0221@gmail.com>
2018-05-14 15:36:13 +0900

    mysql connect

---
+++ db_info.js
@@ -0,0 +1,27 @@
+module.exports = (function () {
+
+    return {
+        local: {
+            host: 'ubuntu@ec2-52-78-135-92.ap-northeast-2.compute.amazonaws.com',
+            port: '3306',
+            user: 'root',
+            password: '123123',
+            database: 'NodeChat',
+        },
+        real: {
+            host: 'ubuntu@ec2-52-78-135-92.ap-northeast-2.compute.amazonaws.com',
+            port: '3306',
+            user: 'root',
+            password: '123123',
+            database: 'NodeChat',
+        },
+        dev: {
+            host: 'ubuntu@ec2-52-78-135-92.ap-northeast-2.compute.amazonaws.com',
+            port: '3306',
+            user: 'root',
+            password: '123123',
+            database: 'NodeChat',
+        }
+    }
+
+})
