ce87b5e8078c4d08e7de1764c6c0c318ddb282bb
kluge121 <kluge0221@gmail.com>
2018-05-26 18:18:34 +0900

    tmp jwt login implementation
    Signed-off-by: kluge121 <kluge0221@gmail.com>

--- route/index.js
+++ route/index.js
@@ -32,8 +32,8 @@ router.post('/login', (req, res) => {
         } else if (accessToken === 'null') { // 토큰어 없다 -> 다시로그인 or 회원가입
 
             if (result.length === 0) {  // 회원가입이 안되어있다면
-                let data = [nickname, accessToken];
-                connection.query('insert into user values (?,?)', data, (err, result) => {
+                let data = [nickname];
+                connection.query('insert into user values (?)', data, (err, result) => {
                     if (err) {
                         console.log('회원가입 오류 ' + err);
                     }
